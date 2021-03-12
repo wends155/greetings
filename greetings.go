3d27842a1053a73b2a7460af7cdc56882edffb8b
wends155 <saliganw@gmail.com>
2021-03-12 20:52:53 +0800

    test for ssh push

--- greetings.go
+++ greetings.go
@@ -2,7 +2,7 @@ package greetings
 
 import "fmt"
 
-// add comment .. testing changes
+// add comment .. testing changes ... testing for ssh push
 func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome Aboard !", name)
 	return message
