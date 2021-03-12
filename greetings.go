25aa3a92d81d2d6d74da5b6cf4fd985262ee4a4f
wends155 <saliganw@gmail.com>
2021-03-12 20:44:39 +0800

    test changes

--- greetings.go
+++ greetings.go
@@ -2,7 +2,7 @@ package greetings
 
 import "fmt"
 
-// add comment
+// add comment .. testing changes
 func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome Aboard !", name)
 	return message
